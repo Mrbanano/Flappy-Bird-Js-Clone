a85050ee0aa654f32d1b0c9d6a03f2a0641fbb4c
Alvaro <6666alvaro666@gmail.com>
2020-04-11 13:19:09 -0500

    add load img and pre- configuration

--- juego.js
+++ juego.js
@@ -7,3 +7,40 @@ var CANVAS_WIDTH = 300;
 var CANVAS_HEIGHT = 530;
 contexto.width = WIDTH;
 contexto.height = HEIGHT;
+
+
+var score = 0
+var FPS =60
+var gravedad = 1.5
+var personaje = {
+    x:50,
+    y:150,
+    w:50,
+    h:50
+}
+var tuberias = new Array()
+tuberias[0] = {
+    x:contexto.width,
+    y:0
+}
+
+//VARIABLES AUDIOS
+
+var punto = new Audio()
+punto.src = "audios/punto.mp3"
+
+//VARIABLES IMAGENES
+var bird = new Image()
+bird.src = "imagenes/bird.png"
+
+var background = new Image()
+background.src = "imagenes/background.png"
+
+var tuberiaNorte = new Image()
+tuberiaNorte.src = "imagenes/tuberiaNorte.png"
+
+var tuberiaSur = new Image()
+tuberiaSur.src = "imagenes/tuberiaSur.png"
+
+var suelo = new Image()
+suelo.src = "imagenes/suelo.png"
