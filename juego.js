8f779791deb1d99aff1598f8e8edc7db35b874d9
Alvaro <6666alvaro666@gmail.com>
2020-04-11 13:18:11 -0500

    add settings

---
+++ juego.js
@@ -0,0 +1,9 @@
+// Settings
+var contexto = document.getElementById("lienzoJuego")
+var ctx = contexto.getContext("2d")
+var WIDTH = 300;
+var HEIGHT = 530;
+var CANVAS_WIDTH = 300;
+var CANVAS_HEIGHT = 530;
+contexto.width = WIDTH;
+contexto.height = HEIGHT;
