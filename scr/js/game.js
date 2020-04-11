9c2c4c13838a3c2ca6c978890318a7ac7a8c068f
Alvaro <6666alvaro666@gmail.com>
2020-04-11 17:01:30 -0500

    add platform and brid

--- scr/js/game.js
+++ scr/js/game.js
@@ -25,6 +25,57 @@ const bg = {
     }
     
 }
+// FOREGROUND
+const fg = {
+    sX: 276,
+    sY: 0,
+    w: 224,
+    h: 112,
+    x: 0,
+    y: cvs.height - 112,
+    
+    dx : 2,
+    
+    draw : function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+}
+
+//BIRD
+const bird = {
+    animation : [
+        {sX: 276, sY : 112},
+        {sX: 276, sY : 139},
+        {sX: 276, sY : 164},
+        {sX: 276, sY : 139}
+    ],
+    x : 50,
+    y : 150,
+    w : 34,
+    h : 26,
+    
+    radius : 12,
+    
+    frame : 0,
+    
+    gravity : 0.25,
+    jump : 4.6,
+    speed : 0,
+    rotation : 0,
+    
+    draw : function(){
+        let bird = this.animation[this.frame];
+        
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.rotation);
+        ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h,- this.w/2, - this.h/2, this.w, this.h);
+        
+        ctx.restore();
+    }
+}
 
 
 
@@ -35,6 +86,8 @@ function draw(){
     ctx.fillStyle="rgba(112, 197, 206,0.5)"; 
     ctx.fillRect(0,0,cvs.width,cvs.height)
     bg.draw();
+    fg.draw();
+    bird.draw();
 }
 //UPDATE
 function update(){
